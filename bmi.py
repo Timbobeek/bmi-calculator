110237fa0615326c8d7d50aed952ecb355aa36a1
Tim Goloshchapov <tim.goloschapov@gmail.com>
2021-02-07 19:47:36 -0600

    initial version

---
+++ bmi.py
@@ -0,0 +1,16 @@
+#option1
+
+#bmi formula = weight(kg) / height(m)^2 
+def bmi_formula(weight, height):
+    return weight**height
+
+bmi_formula(67, 1.57)
+
+
+#def weight(value1):
+#    print(value1)
+
+#def height(value2):
+#    print(value2)
+
+#bmi_formula(weight, height) 
