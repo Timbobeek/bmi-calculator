66c6b7561467febb4214c42184f7f87559304770
Tim Goloshchapov <tim.goloschapov@gmail.com>
2021-02-07 19:49:08 -0600

    added script inputs

--- bmi.py
+++ bmi.py
@@ -1,16 +1,15 @@
-#option1
+#option2
 
 #bmi formula = weight(kg) / height(m)^2 
-def bmi_formula(weight, height):
-    return weight**height
 
-bmi_formula(67, 1.57)
+weight = input("Enter your weight in kg\n>")
 
+height = input("Enter your height in m\n>")
 
-#def weight(value1):
-#    print(value1)
+def formula(bmi):
+    print(bmi)
 
-#def height(value2):
-#    print(value2)
+formula(float(weight)/float(height)**2)
 
-#bmi_formula(weight, height) 
+#now, make it so that if one of the values is not entered 
+# it shows "you're probably fat" message
